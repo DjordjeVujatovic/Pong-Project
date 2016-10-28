3535809e897329bdc2059eefa698f8c614370194
Djordje Vujatovic <djordje_v@hotmail.com>
2016-10-28 12:13:14 -0700

    Added paddle collision.

--- src/ball.js
+++ src/ball.js
@@ -17,6 +17,30 @@ export default class Ball {
     context.closePath();
 
   }
+  paddleCollision (p1,p2) {
+
+    if (this.vx > 0) {
+      
+      const inRightEnd  = this.x >= p2.x;
+
+
+      if (inRightEnd) {
+        if(this.y >= p2.y && this.y <=(p2.y + p2.height)){
+          this.vx *= -1;
+        }
+      }
+
+    } else {
+      const inLeftEnd = this.x <= p1.x + p1.width;
+
+      if (inLeftEnd) {
+        if (this.y >= p1.y && this.y <= (p1.y + p1.height)) {
+          this.vx *= -1;
+        }
+      }
+    }
+  }
+
   bounce() {
     if (this.y <= 0 + this.radius || this.y >= 150 - this.radius) {
         this.vy *= -1
@@ -25,11 +49,14 @@ export default class Ball {
         this.vx *= -1
     }
   }
-     render(context) {
+     render(context, p1, p2) {
      this.x += this.vx;
      this.y += this.vy;
-     this.draw(context);
+     this.paddleCollision(p1, p2);
      this.bounce();
+     this.draw(context);
+     
+     
 
 }
 }
